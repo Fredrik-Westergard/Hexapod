bfe8a31f6c3a994c6b441dee1e6aec03f80e545e
Fredrik-Westergard <fredrik.westergard@ha.ax>
2021-09-07 20:47:22 +0300

    robot now also moves in X axis translation

--- hex/Leg.cpp
+++ hex/Leg.cpp
@@ -28,9 +28,14 @@ void Leg::calculateAngles(double zOffset, double yOffset, double xOffset){
   int yNeg[6] = {1,1,1,1,1,1};
   
   double yA[6];
-  double y[6];
+  double yLen[6];
   double yOt[6];
+
+  double xA[6];
+  double xLen[6];
   double xOt[6];
+
+  double yAd[6];
   
   for(int i=0; i<6; i++){
     xNeg[i] = (xOffset+xOf[i]<=0)?-1:1;
@@ -38,35 +43,59 @@ void Leg::calculateAngles(double zOffset, double yOffset, double xOffset){
 
     yOt[i] = (yOffset+yOf[i])*yNeg[i];
     xOt[i] = (xOffset+xOf[i])*xNeg[i];
-    Serial.println(yOt[i]);
   }
 
-  y[0] = lawOfCosinesSAS(yOt[0],LEGLEN,(45*yNeg[0])+90);
-  y[1] = lawOfCosinesSAS(yOt[1],LEGLEN,90*yNeg[1]);
-  y[2] = lawOfCosinesSAS(yOt[2],LEGLEN,(45*yNeg[2]*-1)+90);
-  y[3] = lawOfCosinesSAS(yOt[3],LEGLEN,(45*yNeg[3])+90);
-  y[4] = lawOfCosinesSAS(yOt[4],LEGLEN,90*yNeg[4]);
-  y[5] = lawOfCosinesSAS(yOt[5],LEGLEN,(45*yNeg[5]*-1)+90);
+  yLen[0] = lawOfCosinesSAS(yOt[0],LEGLEN,(45*yNeg[0])+90);
+  yLen[1] = lawOfCosinesSAS(yOt[1],LEGLEN,90*yNeg[1]);
+  yLen[2] = lawOfCosinesSAS(yOt[2],LEGLEN,(45*yNeg[2]*-1)+90);
+  yLen[3] = lawOfCosinesSAS(yOt[3],LEGLEN,(45*yNeg[3])+90);
+  yLen[4] = lawOfCosinesSAS(yOt[4],LEGLEN,90*yNeg[4]);
+  yLen[5] = lawOfCosinesSAS(yOt[5],LEGLEN,(45*yNeg[5]*-1)+90);
 
-  yA[0] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[0],yOt[0]))*yNeg[0]*-1;
-  yA[1] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[1],yOt[1]))*yNeg[1]*-1;
-  yA[2] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[2],yOt[2]))*yNeg[2]*-1;
-  yA[3] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[3],yOt[3]))*yNeg[3];
-  yA[4] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[4],yOt[4]))*yNeg[4];
-  yA[5] = degreesConverter(lawOfCosinesSSS(LEGLEN,y[5],yOt[5]))*yNeg[5];
+  yA[0] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[0],yOt[0]))*yNeg[0]*-1;
+  yA[1] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[1],yOt[1]))*yNeg[1]*-1;
+  yA[2] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[2],yOt[2]))*yNeg[2]*-1;
+  yA[3] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[3],yOt[3]))*yNeg[3];
+  yA[4] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[4],yOt[4]))*yNeg[4];
+  yA[5] = degreesConverter(lawOfCosinesSSS(LEGLEN,yLen[5],yOt[5]))*yNeg[5];
 
-  Serial.println(y[0]);
-  Serial.println(yA[0]);
+  yAd[0] = 90+(degreesConverter(asin((sin(((45*yNeg[0])+90)/(180.0/M_PI))*LEGLEN)/yLen[0]))*xNeg[0]);
+  yAd[1] = 90+(degreesConverter(asin((sin((90*yNeg[1])/(180.0/M_PI))*LEGLEN)/yLen[1]))*xNeg[1]);
+  yAd[2] = 90+(degreesConverter(asin((sin(((45*yNeg[2])+90)/(180.0/M_PI))*LEGLEN)/yLen[2]))*xNeg[2]);
+  yAd[3] = 90+(degreesConverter(asin((sin(((45*yNeg[3])+90)/(180.0/M_PI))*LEGLEN)/yLen[3]))*xNeg[3]*-1);  
+  yAd[4] = 90+(degreesConverter(asin((sin((90*yNeg[4])/(180.0/M_PI))*LEGLEN)/yLen[4]))*xNeg[4]*-1);
+  yAd[5] = 90+(degreesConverter(asin((sin(((45*yNeg[5])+90)/(180.0/M_PI))*LEGLEN)/yLen[5]))*xNeg[5]*-1);
+
+  xLen[0] = lawOfCosinesSAS(xOt[0],yLen[0],yAd[0]);
+  xLen[1] = lawOfCosinesSAS(xOt[1],yLen[1],yAd[1]);
+  xLen[2] = lawOfCosinesSAS(xOt[2],yLen[2],yAd[2]);
+  xLen[3] = lawOfCosinesSAS(xOt[3],yLen[3],yAd[3]);
+  xLen[4] = lawOfCosinesSAS(xOt[4],yLen[4],yAd[4]);
+  xLen[5] = lawOfCosinesSAS(xOt[5],yLen[5],yAd[5]);
+
+  Serial.println(xLen[0]);
+  Serial.println(xLen[1]);
+  Serial.println(xLen[2]);
+  Serial.println(xLen[3]);
+  Serial.println(xLen[4]);
+  Serial.println(xLen[5]);
+
+  xA[0] = yA[0]-(degreesConverter(lawOfCosinesSSS(xLen[0],yLen[0],xOt[0]))*xNeg[0]*-1);
+  xA[1] = yA[1]-(degreesConverter(lawOfCosinesSSS(xLen[1],yLen[1],xOt[1]))*xNeg[1]);
+  xA[2] = yA[2]-(degreesConverter(lawOfCosinesSSS(xLen[2],yLen[2],xOt[2]))*xNeg[2]);
+  xA[3] = yA[3]-(degreesConverter(lawOfCosinesSSS(xLen[3],yLen[3],xOt[3]))*xNeg[3]*-1);
+  xA[4] = yA[4]-(degreesConverter(lawOfCosinesSSS(xLen[4],yLen[4],xOt[4]))*xNeg[4]*-1);
+  xA[5] = yA[5]-(degreesConverter(lawOfCosinesSSS(xLen[5],yLen[5],xOt[5]))*xNeg[5]);
   
   for(int i=0; i<6; i++){
-    double dLen = calculateDesiredLength(zOffset-zOf[i], y[i]);
+    double dLen = calculateDesiredLength(zOffset-zOf[i], xLen[i]);
     
     setAngleA(degreesConverter(lawOfCosinesSSS(TIBIA,SHIN,dLen)),i);
     setAngleB(
-      360-(90+(degreesConverter(asin(y[i]/dLen))
+      360-(90+(degreesConverter(asin(xLen[i]/dLen))
       +degreesConverter(lawOfCosinesSSS(dLen,TIBIA,SHIN)))),i);
       setAngleC(0,i);
-    setAngleC(yA[i],i);
+    setAngleC(xA[i],i);
   }
 }
 
